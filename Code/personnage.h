370025f8165dca1064cf8683317eae4436c134ac
Julien Payot <julien.payot@yahoo.fr>
2016-04-26 19:07:33 +0200

    Update personnage.h

--- Code/personnage.h
+++ Code/personnage.h
@@ -8,13 +8,13 @@ using namespace std;
         void affiche(void);                                                                 //récupère les attribut de CPersonne
         //void attaquer();
         //void changerArme();
-        bool changementvie (unsigned short vie1);                                            //change la vie de CPersonne, si <0 renvoie 1 car mort
+        bool changementVie (unsigned short vie1);                                            //change la vie de CPersonne, si <0 renvoie 1 car mort
         bool estVivant ();                                                                  // Vivant ou mort
         unsigned short degat (unsigned short force1);                                       //calcul des dégats : force ennemie-defense=vie perdu
-        unsigned short getvie();
-        unsigned short getforce();
-        unsigned short getdefense();
-        string getnom();
+        unsigned short getVie();
+        unsigned short getForce();
+        unsigned short getDefense();
+        string getNom();
 
     private:
 
