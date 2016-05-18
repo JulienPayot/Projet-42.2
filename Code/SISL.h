f8674e788746716d3aacc494719fcbf69d17b493
Asphox <aspirine026@gmail.com>
2016-05-18 09:46:40 +0200

    Update SISL.h

--- Code/SISL.h
+++ Code/SISL.h
@@ -1,11 +1,11 @@
 /*<===========================================================
 LIB  : SISL ( SIgnal SLot)
-version : 1.1
-
-ajouts 1.1 =>
+version : 1.2
+ajouts 1.2 =>
     -ajout de SIGNAL_2 et SLOT_2
     -ajout d'une verification avant d'appeler un slot en interne ( verification que le slot est bien attaché a une fonction )
     -ajout d'une methode "detach" qui retire la fonction attacher au slot
+    -possibilité de connecter des signaux à un signal
 
 DEV : SOTON "Asphox" Dylan
 Date : 06/05/2016
@@ -131,21 +131,33 @@ namespace sisl
     {
     private:
         std::vector<const SLOT_0*> _array_slot;
+        std::vector<const SIGNAL_0*> _array_signal_0;
     public:
         void connect(const SLOT_0* slot){
             _array_slot.push_back(slot);
+        }
+        void connect(const SIGNAL_0* signal){
+            _array_signal_0.push_back(signal);
         }
         void disconnect(const SLOT_0* slot){
             size_t i=0;
             while(_array_slot[i] != slot && i<_array_slot.size())i++;
             if(i<_array_slot.size())_array_slot.erase(_array_slot.begin()+i);
         }
+        void disconnect(const SIGNAL_0* signal){
+            size_t i=0;
+            while(_array_signal_0[i] != signal && i<_array_signal_0.size())i++;
+            if(i<_array_signal_0.size())_array_signal_0.erase(_array_signal_0.begin()+i);
+        }
         void disconnect_all(){
             _array_slot.clear();
+            _array_signal_0.clear();
         }
         void emit() const {
             for(size_t i=0;i<_array_slot.size();i++)
                 _array_slot[i]->exec();
+            for(size_t i=0;i<_array_signal_0.size();i++)
+                _array_signal_0[i]->emit();
         }
     };
 
@@ -156,24 +168,63 @@ namespace sisl
     class SIGNAL_1          //signal with 1 arg
     {
     private:
-        std::vector<SLOT_1<P>*> _array_slot;
+        std::vector<const SLOT_1<P>*> _array_slot;
+        std::vector<const SLOT_0*> _array_slot_0;
+        std::vector<const SIGNAL_0*> _array_signal_0;
+        std::vector<const SIGNAL_1<P>*> _array_signal_P;
     public:
 
-        void connect(SLOT_1<P>* slot){
+        void connect(const SLOT_0* slot){
+            _array_slot_0.push_back(slot);
+        }
+        void connect(const SLOT_1<P>* slot){
             _array_slot.push_back(slot);
         }
+        void connect(const SIGNAL_0* signal){
+            _array_signal_0.push_back(signal);
+        }
+        void connect(const SIGNAL_1<P>* signal){
+            _array_signal_P.push_back(signal);
+        }
 
-        void disconnect(SLOT_1<P>* slot){
+        void disconnect(const SLOT_0* slot){
+            size_t i=0;
+            while(_array_slot_0[i] != slot && i<_array_slot_0.size())i++;
+            if(i<_array_slot_0.size())_array_slot_0.erase(_array_slot_0.begin()+i);
+        }
+
+        void disconnect(const SLOT_1<P>* slot){
             size_t i=0;
             while(_array_slot[i] != slot && i<_array_slot.size())i++;
             if(i<_array_slot.size())_array_slot.erase(_array_slot.begin()+i);
         }
+
+        void disconnect(const SIGNAL_0* signal){
+            size_t i=0;
+            while(_array_signal_0[i] != signal && i<_array_signal_0.size())i++;
+            if(i<_array_signal_0.size())_array_signal_0.erase(_array_signal_0.begin()+i);
+        }
+
+        void disconnect(const SIGNAL_1<P>* signal){
+            size_t i=0;
+            while(_array_signal_P[i] != signal && i<_array_signal_P.size())i++;
+            if(i<_array_signal_P.size())_array_signal_P.erase(_array_signal_P.begin()+i);
+        }
         void disconnect_all(){
             _array_slot.clear();
+            _array_slot_0.clear();
+            _array_signal_0.clear();
+            _array_signal_P.clear();
         }
         void emit(const P param) const {
+            for(size_t i=0;i<_array_slot_0.size();i++)
+                _array_slot_0[i]->exec();
             for(size_t i=0;i<_array_slot.size();i++)
                 _array_slot[i]->exec(param);
+            for(size_t i=0;i<_array_signal_0.size();i++)
+                _array_signal_0[i]->emit();
+            for(size_t i=0;i<_array_signal_P.size();i++)
+                _array_signal_P[i]->emit(param);
         }
 
     };
@@ -185,54 +236,69 @@ namespace sisl
     class SIGNAL_2          //signal with 1 arg
     {
     private:
-        std::vector<SLOT_2<P1,P2>*> _array_slot;
+        std::vector<const SLOT_0*> _array_slot_0;
+        std::vector<const SLOT_2<P1,P2>*> _array_slot;
+        std::vector<const SIGNAL_0*> _array_signal_0;
+        std::vector<const SIGNAL_2<P1,P2>*> _array_signal_P;
     public:
 
-        void connect(SLOT_2<P1,P2>* slot){
+        void connect(const SLOT_0* slot){
+            _array_slot_0.push_back(slot);
+        }
+
+        void connect(const SLOT_2<P1,P2>* slot){
             _array_slot.push_back(slot);
         }
 
-        void disconnect(SLOT_2<P1,P2>* slot){
+        void connect(const SIGNAL_0* signal){
+            _array_signal_0.push_back(signal);
+        }
+
+        void connect(const SIGNAL_2<P1,P2>* signal){
+            _array_signal_P.push_back(signal);
+        }
+
+        void disconnect(const SLOT_0* slot){
+            size_t i=0;
+            while(_array_slot_0[i] != slot && i<_array_slot_0.size())i++;
+            if(i<_array_slot_0.size())_array_slot_0.erase(_array_slot_0.begin()+i);
+        }
+
+        void disconnect(const SIGNAL_0* signal){
+            size_t i=0;
+            while(_array_signal_0[i] != signal && i<_array_signal_0.size())i++;
+            if(i<_array_signal_0.size())_array_signal_0.erase(_array_signal_0.begin()+i);
+        }
+
+        void disconnect(const SIGNAL_2<P1,P2>* signal){
+            size_t i=0;
+            while(_array_signal_P[i] != signal && i<_array_signal_P.size())i++;
+            if(i<_array_signal_P.size())_array_signal_P.erase(_array_signal_P.begin()+i);
+        }
+
+        void disconnect(const SLOT_2<P1,P2>* slot){
             size_t i=0;
             while(_array_slot[i] != slot && i<_array_slot.size())i++;
             if(i<_array_slot.size())_array_slot.erase(_array_slot.begin()+i);
         }
         void disconnect_all(){
+            _array_slot_0.clear();
             _array_slot.clear();
+            _array_signal_0.clear();
+            _array_signal_P.clear();
         }
         void emit(const P1 param1,const P2 param2) const {
+            for(size_t i=0;i<_array_slot_0.size();i++)
+                _array_slot_0[i]->exec();
             for(size_t i=0;i<_array_slot.size();i++)
                 _array_slot[i]->exec(param1,param2);
+            for(size_t i=0;i<_array_signal_0.size();i++)
+                _array_signal_0[i]->emit();
+            for(size_t i=0;i<_array_signal_P.size();i++)
+                _array_signal_P[i]->emit(param1,param2);
         }
 
     };
-    /*==================================================================
-        CONNECT FUNCTIONS
-      ==================================================================*/
-
-    void connect(SIGNAL_0* sig, SLOT_0* slot){
-        sig->connect(slot);
-    }
-    template<typename P>
-    void connect(SIGNAL_1<P>* sig, SLOT_1<P>* slot){
-        sig->connect(slot);
-    }
-    template<typename P1, typename P2>
-    void connect(SIGNAL_2<P1,P2>* sig, SLOT_2<P1,P2>* slot){
-        sig->connect(slot);
-    }
-    void disconnect(SIGNAL_0* sig, SLOT_0* slot){
-        sig->disconnect(slot);
-    }
-    template<typename P>
-    void disconnect(SIGNAL_1<P>* sig, SLOT_1<P>* slot){
-        sig->disconnect(slot);
-    }
-    template<typename P1,typename P2>
-    void disconnect(SIGNAL_2<P1,P2>* sig, SLOT_2<P1,P2>* slot){
-        sig->disconnect(slot);
-    }
-
 }
 
 #endif // SISL_H
