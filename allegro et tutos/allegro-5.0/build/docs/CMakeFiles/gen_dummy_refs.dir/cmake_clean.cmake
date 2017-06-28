33f9b1be4eaa1fc3b63e92518568754687eac583
JulienPayot <julien.payot@yahoo.fr>
2017-06-28 22:16:07 +0200

    pour planter le depot

--- allegro et tutos/allegro-5.0/build/docs/CMakeFiles/gen_dummy_refs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gen_dummy_refs"
-  "dummy_refs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gen_dummy_refs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- allegro et tutos/allegro-5.0/build/tests/CMakeFiles/copy_tests_example_data.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_tests_example_data"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_tests_example_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
